8edadfaa5560fdae6bcc8c3353583c80c0485ac1
Kammad <dmtrsmirn@yandex.ru>
2024-04-19 18:13:23 +0300

    Home screen and some assets added

---
+++ Screens/BottomPanel.js
@@ -0,0 +1,132 @@
+import React, { useState } from 'react';
+import { View, TouchableOpacity, Text, StyleSheet, Animated, Dimensions, SafeAreaView, Image } from 'react-native';
+
+const screenWidth = Dimensions.get("window").width;
+
+const BottomPanelToggle = () => {
+  const [isPanelVisible, setPanelVisible] = useState(false);
+  const slideAnimation = new Animated.Value(1);
+
+  const togglePanel = () => {
+    if (isPanelVisible) {
+      Animated.timing(slideAnimation, {
+        toValue: 0,
+        duration: 300,
+        useNativeDriver: true,
+      }).start(() => setPanelVisible(false));
+    } else {
+      setPanelVisible(true);
+      Animated.timing(slideAnimation, {
+        toValue: 1,
+        duration: 300,
+        useNativeDriver: true,
+      }).start();
+    }
+  };
+
+  const panelTranslateY = slideAnimation.interpolate({
+    inputRange: [0, 1],
+    outputRange: [200, 0],
+  });
+
+  return (
+    <View style={styles.container}>
+
+      {/* Bottom Panel */}
+      {isPanelVisible && (
+        <Animated.View
+          style={[styles.bottomPanel, { transform: [{ translateY: panelTranslateY }]}]}>
+          {/* Close bottom panel button */}
+          <TouchableOpacity onPress={togglePanel} style={[{alignSelf: 'center', height: 20, width: 100}]}>
+            <Image style={[{height: 12, width: 20, resizeMode: 'stretch', alignSelf: 'center'}]} source={require('../assets/ArrowDown.png')}/>
+          </TouchableOpacity>
+          
+          {/* Content of the bottom panel*/}
+          <SafeAreaView style={styles.bottomButtonWrapper}>
+            <TouchableOpacity style={styles.bottomPanelButton}>
+              <Image style={[styles.bottomPanelButton, {resizeMode: 'stretch', position: 'absolute', top: -15, left: -15}]} source={require('../assets/Add button.png')}></Image>
+            </TouchableOpacity>
+            <View style={styles.bottomBalancePanel}>
+              <Text style={[{fontSize: 16, fontWeight: 'bold'}]}>Balance: </Text>
+            </View>
+            <TouchableOpacity style={styles.bottomPanelButton}>
+              <Image style={[styles.bottomPanelButton, {resizeMode: 'stretch', position: 'absolute', top: -15, left: -15}]} source={require('../assets/Minus button.png')}></Image>
+            </TouchableOpacity>
+          </SafeAreaView>
+        </Animated.View>
+      )}
+
+      {/* Toggle Button */}
+      {!isPanelVisible && (
+        <TouchableOpacity onPress={togglePanel} style={styles.toggleButton}>
+          <Image style={[{height: 12, width: 20, resizeMode: 'stretch'}]} source={require('../assets/ArrowUp.png')}></Image>
+        </TouchableOpacity>
+      )}
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'flex-end', // Aligns items at the bottom
+    alignItems: 'center',
+  },
+  mainContent: {
+    flex: 1, // Takes up remaining space
+    justifyContent: 'center',
+    alignItems: 'center',
+    zIndex: 1,
+  },
+  mainText: {
+    fontSize: 20,
+  },
+  bottomPanel: {
+    flex: 1,
+    flexDirection: 'column',
+    borderTopRightRadius: 15,
+    borderTopLeftRadius: 15,
+    position: 'absolute',
+    bottom: 0,
+    left: -screenWidth/2,
+    right: 0,
+    backgroundColor: '#d0d0d0',
+    padding: 20,
+    width: screenWidth,
+  },
+  panelText: {
+    fontSize: 18,
+  },
+  toggleButton: {
+    justifyContent: 'center',
+    backgroundColor: 'grey',
+    paddingVertical: 15,
+    paddingHorizontal: 30,
+    borderTopRightRadius: 10,
+    borderTopLeftRadius: 10,
+    zIndex: 1,
+  },
+  buttonText: {
+    color: 'white',
+    fontSize: 16,
+  },
+  bottomButtonWrapper: {
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'space-evenly',
+  },
+  bottomPanelButton: {
+    height: 48,
+    width: 48,
+    margin: 10,
+    borderRadius: 50,
+    
+  },
+  bottomBalancePanel: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginHorizontal: 20,
+  }
+});
+
+export default BottomPanelToggle;
---
+++ Screens/Home.js
@@ -0,0 +1,112 @@
+import React from "react";
+import { View, Text, StyleSheet, FlatList, SafeAreaView} from "react-native";
+import { useState, useRef } from "react";
+import PieChart from "react-native-pie-chart";
+import { Dimensions } from "react-native";
+import BottomPanelToggle from "./BottomPanel";
+
+const screenWidth = Dimensions.get("window").width;
+
+export const Home = () => {
+  const [categories, setCategories] = useState([
+    { title: 'Food', sum: 322, color: "#666666", id: 'c1' },
+    { title: 'Car', sum: 228, color: "#777777", id: 'c2' },
+    { title: 'Pets', sum: 1000, color: "#888888", id: 'c3' },
+    { title: 'Sports', sum: 500, color: "#999999", id: 'c4' },
+    { title: 'Health', sum: 78, color: "#AAAAAA", id: 'c5' },
+  ])
+
+
+  return (
+    <SafeAreaView style={styles.wrapper}>
+      <Chart categories={categories}></Chart>
+      <FlatList
+        style={[{ width: screenWidth, height: 400, borderTopColor: 'grey', borderTopWidth: 2, }]}
+        renderItem={({ item }) => <Item title={item.title} color={item.color} keyExtractor={item => item.id} />}
+        data={categories}
+      />
+      <BottomPanelToggle></BottomPanelToggle>
+    </SafeAreaView>
+  );
+};
+
+const Chart = ({ categories }) => {
+  const widthAndHeight = 200;
+  const cat = [...categories];
+  const series = cat.map(x => x = x.sum);
+  const colors = cat.map(x => x = x.color);
+
+  return (
+    <View style={[styles.container, { padding: 20 }]}>
+      <PieChart style={styles.doughnut}
+        widthAndHeight={widthAndHeight}
+        series={series}
+        sliceColor={colors}
+        coverRadius={0.55}
+        coverFill={'#FFF'}
+      />
+    </View>
+  );
+}
+
+const Item = ({ title, color }) => {
+  return (
+    <View style={styles.legendItem}>
+      <View style={[styles.legendCircle, { backgroundColor: color }]}></View>
+      <Text style={[{ fontSize: 20 }]}>{title}: %</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  wrapper: {
+    flex: 1,
+    alignItems: 'center',
+    flexDirection: 'column',
+    // justifyContent: 'flex-end',
+    gap: 5,
+  },
+  title: {
+    fontSize: 24,
+    margin: 10,
+  },
+  doughnut: {
+    // position: "relative",
+  },
+  itemframe: {
+    marginVertical: 7,
+    paddingHorizontal: 20,
+    flex: 1,
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    borderRadius: 4,
+    backgroundColor: "#cccccc",
+    width: 270,
+    height: 60,
+  },
+  dateText: {
+    fontSize: 16,
+  },
+  amountText: {
+    fontWeight: 'bold',
+  },
+  bottomPanelButton: {
+    height: 50,
+    width: 50,
+  },
+  legendItem: {
+    flex: 1,
+    flexDirection: 'row',
+    padding: 20,
+    marginVertical: 8,
+    marginHorizontal: 16,
+    alignItems: 'center'
+  },
+  legendCircle: {
+    height: 16,
+    width: 16,
+    borderRadius: 50,
+    marginRight: 15,
+  }
+});
